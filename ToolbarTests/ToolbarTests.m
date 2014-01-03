31d3ab4f875dc32dcff300a3c8eff02073eb93b4
saerem21 <nemessisdds>
2014-01-03 14:09:51 +0900

    Initial Commit

---
+++ ToolbarTests/ToolbarTests.m
@@ -0,0 +1,34 @@
+//
+//  ToolbarTests.m
+//  ToolbarTests
+//
+//  Created by SDT-1 on 2014. 1. 3..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ToolbarTests : XCTestCase
+
+@end
+
+@implementation ToolbarTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
